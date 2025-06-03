6169dd6996de75b13f887ca57d85ba7517f9656a
GiulianoDiLorenzo <giuliano.dilorenzo@mail.polimi.it>
2025-06-03 17:20:28 +0200

    Update mems_spk_WD.m
    It's the end finally

--- mems_spk_WD.m
+++ mems_spk_WD.m
@@ -1,3 +1,7 @@
+%% SOUND SYNTHESIS AND SPATIAL PROCESSING - Homework
+% Giuliano Di Lorenzo - 242712
+% Nicola Mugnaini - 243211
+
 clear; close all; clc
 
 %% Simscape File for Ground Truth
@@ -74,7 +78,7 @@ L3 = Ltube2*gamma2^2;
 Fin = alpha * vin;
 
 %% Setting of Free Parameters (Adaptation Conditions)
-%Single elements impedance
+% Passive elements impedance
 Z_R1 = R1;
 Z_R2 = R2;
 Z_R3 = R3;
@@ -86,7 +90,7 @@ Z_L1 = (2*L1)/Ts;
 Z_L2 = (2*L2)/Ts;
 Z_L3 = (2*L3)/Ts;
 
-%Adaption Conditions
+% Adaption conditions - Passive elements
 Z2 = Z_R1;
 Z5 = Z_C1;
 Z8 = Z_R2;
@@ -98,6 +102,7 @@ Z23 = Z_C4;
 Z26 = Z_L3;
 Z27 = Z_R3;
 
+% Adaption conditions - Adaptors
 Z25 = Z26 + Z27;
 Z24 = Z25;
 Z22 = (Z23*Z24)/(Z23 + Z24);
@@ -118,14 +123,12 @@ Z1 = Z2 + Z3;
 
 
 %% Computing Scattering Matrices
-Bser = [1,1,1];
-Bsermin = [-1,1,1];
-Qpar = [1,1,1];
+B = [1,1,1];
+Q = [1,1,1];
 
 Zpar1 = diag([Z4,Z5,Z6]);
 Zpar2 = diag([Z16,Z17,Z18]);
 Zpar3 = diag([Z22,Z23,Z24]);
-
 
 Zser1 = diag([Z1,Z2,Z3]);
 Zser2 = diag([Z7,Z8,Z9]);
@@ -134,17 +137,16 @@ Zser4 = diag([Z13,Z14,Z15]);
 Zser5 = diag([Z19,Z20,Z21]);
 Zser6 = diag([Z25,Z26,Z27]);
 
-Sser1= eye(3) - 2*Zser1*Bser'*inv(Bser*Zser1*Bser')*Bser;
-Sser2= eye(3) - 2*Zser2*Bser'*inv(Bser*Zser2*Bser')*Bser;
-Sser3= eye(3) - 2*Zser3*Bser'*inv(Bser*Zser3*Bser')*Bser;
-%Sser4= eye(3) - 2*Zser4*Bsermin'*inv(Bsermin*Zser4*Bsermin')*Bsermin;
-Sser4= eye(3) - 2*Zser4*Bser'*inv(Bser*Zser4*Bser')*Bser;
-Sser5= eye(3) - 2*Zser5*Bsermin'*inv(Bsermin*Zser5*Bsermin')*Bsermin;
-Sser6= eye(3) - 2*Zser6*Bsermin'*inv(Bsermin*Zser6*Bsermin')*Bsermin;
+S_A = eye(3) - 2*Zser1*B'*inv(B*Zser1*B')*B;
+S_C = eye(3) - 2*Zser2*B'*inv(B*Zser2*B')*B;
+S_D = eye(3) - 2*Zser3*B'*inv(B*Zser3*B')*B;
+S_E = eye(3) - 2*Zser4*B'*inv(B*Zser4*B')*B;
+S_G = eye(3) - 2*Zser5*B'*inv(B*Zser5*B')*B;
+S_I = eye(3) - 2*Zser6*B'*inv(B*Zser6*B')*B;
 
-Spar1= 2*Qpar'*inv(Qpar*inv(Zpar1)*Qpar')*Qpar*inv(Zpar1) - eye(3);
-Spar2= 2*Qpar'*inv(Qpar*inv(Zpar2)*Qpar')*Qpar*inv(Zpar2) - eye(3);
-Spar3= 2*Qpar'*inv(Qpar*inv(Zpar3)*Qpar')*Qpar*inv(Zpar3) - eye(3);
+S_B = 2*Q'*inv(Q*inv(Zpar1)*Q')*Q*inv(Zpar1) - eye(3);
+S_F = 2*Q'*inv(Q*inv(Zpar2)*Q')*Q*inv(Zpar2) - eye(3);
+S_H = 2*Q'*inv(Q*inv(Zpar3)*Q')*Q*inv(Zpar3) - eye(3);
 
 %% Initialization of Waves
 a2 = 0;
@@ -172,6 +174,7 @@ Fout = zeros(1, length(t));
 %% Simulation Algorithm
 
 for n = 1 : length(Fin)
+    % Update coefficients
     a5 = b5;
     a11 = -b11;
     a14 = b14;
@@ -181,73 +184,73 @@ for n = 1 : length(Fin)
     a26 = -b26;
 
     % Forward Scan
-    b25 = Sser6(1,:)*[0;a26;a27];
+    b25 = S_I(1,:)*[0;a26;a27];
     a24 = b25;
 
-    b22 = Spar3(1,:)*[0;a23;a24];
+    b22 = S_H(1,:)*[0;a23;a24];
     a21 = b22;
 
-    b19 = Sser5(1,:)*[0;a20;a21];
+    b19 = S_G(1,:)*[0;a20;a21];
     a18 = b19;
 
-    b16 = Spar2(1,:)*[0;a17;a18];
+    b16 = S_F(1,:)*[0;a17;a18];
     a15 = b16;
 
-    b13 = Sser4(1,:)*[0;a14;a15];
+    b13 = S_E(1,:)*[0;a14;a15];
     a12 = b13;
 
-    b10 = Sser3(1,:)*[0;a11;a12];
+    b10 = S_D(1,:)*[0;a11;a12];
     a9 = b10;
     
-    b7 = Sser2(1,:)*[0;a8;a9];
+    b7 = S_C(1,:)*[0;a8;a9];
     a6 = b7;
 
-    b4 = Spar1(1,:)*[0;a5;a6];
+    b4 = S_B(1,:)*[0;a5;a6];
     a3 = b4;
 
-    b1 = Sser1(1,:)*[0;a2;a3];
+    b1 = S_A(1,:)*[0;a2;a3];
 
     % Local Root Scattering
 
-     a_root = b1;
-    b_root = 2*Fin(n) - a_root;
+    a_g = b1;
+    b_g = 2*Fin(n) - a_g;
 
     % Backward Scan
-    a1 = b_root;
-    b2 = Sser1(2,:)*[a1;a2;a3];
-    b3 = Sser1(3,:)*[a1;a2;a3];
+    a1 = b_g;
+    b2 = S_A(2,:)*[a1;a2;a3];
+    b3 = S_A(3,:)*[a1;a2;a3];
     a4 = b3;
 
-    b5 = Spar1(2,:)*[a4;a5;a6];
-    b6 = Spar1(3,:)*[a4;a5;a6];
+    b5 = S_B(2,:)*[a4;a5;a6];
+    b6 = S_B(3,:)*[a4;a5;a6];
     a7 = b6;
 
-    b8 = Sser2(2,:)*[a7;a8;a9];
-    b9 = Sser2(3,:)*[a7;a8;a9];
+    b8 = S_C(2,:)*[a7;a8;a9];
+    b9 = S_C(3,:)*[a7;a8;a9];
     a10 = b9;
 
-    b11 = Sser3(2,:)*[a10;a11;a12];
-    b12 = Sser3(3,:)*[a10;a11;a12];
+    b11 = S_D(2,:)*[a10;a11;a12];
+    b12 = S_D(3,:)*[a10;a11;a12];
     a13 = b12;
 
-    b14 = Sser4(2,:)*[a13;a14;a15];
-    b15 = Sser4(3,:)*[a13;a14;a15];
+    b14 = S_E(2,:)*[a13;a14;a15];
+    b15 = S_E(3,:)*[a13;a14;a15];
     a16 = b15;
 
-    b17 = Spar2(2,:)*[a16;a17;a18];
-    b18 = Spar2(3,:)*[a16;a17;a18];
+    b17 = S_F(2,:)*[a16;a17;a18];
+    b18 = S_F(3,:)*[a16;a17;a18];
     a19 = b18;
 
-    b20 = Sser5(2,:)*[a19;a20;a21];
-    b21 = Sser5(3,:)*[a19;a20;a21];
+    b20 = S_G(2,:)*[a19;a20;a21];
+    b21 = S_G(3,:)*[a19;a20;a21];
     a22 = b21;
 
-    b23 = Spar3(2,:)*[a22;a23;a24];
-    b24 = Spar3(3,:)*[a22;a23;a24];
+    b23 = S_H(2,:)*[a22;a23;a24];
+    b24 = S_H(3,:)*[a22;a23;a24];
     a25 = b24;
 
-    b26 = Sser6(2,:)*[a25;a26;a27];
-    b27 = Sser6(3,:)*[a25;a26;a27];
+    b26 = S_I(2,:)*[a25;a26;a27];
+    b27 = S_I(3,:)*[a25;a26;a27];
 
     % Read Output
     Fout(n) = (a27+b27)/2;
